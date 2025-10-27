7e6e1513cc7b43a2fd5c08f37c3fb171eac7ee24
mominul37 <mominul.islam.3725@gmail.com>
2025-10-28 00:19:10 +0600

    to create onclick

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import  './country.css'
 
 const Country = ({country}) => {
     console.log(country.languages.languages)
+
+    const handleVisited = () =>{
+        console.log('btn click')
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.png}/>
             <h1>Name:{country.name.common}</h1>
             <p>Population:{country.population.population}</p>
             <h2>Capital:{country.capital.capital}</h2>
             <h3>language:{country.languages.languages.eng}</h3>
-            <p>Area:{country.area.area}</p>
+            <p>Area:{country.area.area} {country.area.area > 300000 ? "Big Country":"Small Country"}</p>
             <p>CCN3:{country.ccn3.ccn3}</p>
             <h4>Region:{country.region.region}</h4>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,18 +1,22 @@
 
 import React ,{ use } from 'react'
 import Country from '../Country/Country'
+import './countries.css'
 
 const Countries = ({countriesPromise}) => {
     const CountriesData =use(countriesPromise);
     const Countries = CountriesData.countries;
     console.log(Countries)
     return (
-        <div>
+        <div className='country1'>
             <h1>In the countries:{Countries.length}</h1>
+           <div className='countries'>
             {
                 Countries.map(country => <Country country={country}></Country>)
             }
+           </div> 
         </div>
+       
     );
 };
 
